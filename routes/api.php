c78affeece037a6f5828f733656fb9bdf7a3a9ad
Mulinge <Mwalavu@users.noreply.github.com>
2021-09-14 14:24:57 +0100

    add README

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\payments\mpesa\MPESAController;
+use App\Http\Controllers\payments\mpesa\MPESAResponsesController;;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('validation', [MPESAResponsesController::class, 'validation']);
+Route::post('confirmation', [MPESAResponsesController::class, 'confirmation']);
+Route::post('stkpush', [MPESAResponsesController::class, 'stkPush']);
+Route::post('b2ccallback', [MPESAResponsesController::class, 'b2cCallback']);
+Route::post('transaction-status/result_url', [MPESAResponsesController::class, 'transactionStatusResponse']);
+Route::post('reversal/result_url', [MPESAResponsesController::class, 'transactionReversal']);
